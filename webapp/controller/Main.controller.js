d6f1c9cfcb8f9400a1f739ae5c6e3d4a318501f8
purnima bandi <22ht1a0509@city.ac.in>
2025-12-15 05:49:29 +0000

    Initial commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.purnima.project1.project1.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/purnima/project1/project1/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
